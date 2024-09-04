f65c1084887d784cabc0af1809b0efbed9c72d82
Adam Wójs <adam@wojs.pl>
2024-09-04 12:32:50 +0200

    Added bundle structure

---
+++ features/browser/BasicLogin.feature
@@ -0,0 +1,6 @@
+@IbexaOSS
+Feature: An example test checking if user can login and if behat config is proper.
+
+  @javascript
+  Scenario: User can log in as admin user
+    Given I am logged as admin
---
+++ src/bundle/DependencyInjection/IbexaCoreSearchExtension.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\CoreSearch\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
+
+final class IbexaCoreSearchExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * @param array<string, mixed> $configs
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+
+        if ($this->shouldLoadTestServices($container)) {
+            $loader->load('test/pages.yaml');
+            $loader->load('test/components.yaml');
+            $loader->load('test/contexts.yaml');
+        }
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDefaultConfiguration($container);
+        $this->prependJMSTranslation($container);
+    }
+
+    private function prependDefaultConfiguration(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/prepend.yaml';
+
+        $container->addResource(new FileResource($configFile));
+
+        $configs = Yaml::parseFile($configFile, Yaml::PARSE_CONSTANT) ?? [];
+        foreach ($configs as $name => $config) {
+            $container->prependExtensionConfig($name, $config);
+        }
+    }
+
+    private function prependJMSTranslation(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('jms_translation', [
+            'configs' => [
+                'ibexa_core_search' => [
+                    'dirs' => [
+                        __DIR__ . '/../../',
+                    ],
+                    'excluded_dirs' => ['Behat'],
+                    'output_dir' => __DIR__ . '/../Resources/translations/',
+                    'output_format' => 'xliff',
+                ],
+            ],
+        ]);
+    }
+
+    private function shouldLoadTestServices(ContainerBuilder $container): bool
+    {
+        return $container->hasParameter('ibexa.behat.browser.enabled')
+            && true === $container->getParameter('ibexa.behat.browser.enabled');
+    }
+}
